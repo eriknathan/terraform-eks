f94e4681fd9ad4f2040d639e68e6a96e8255f28e
eriknathan <eriknathan.contato@gmail.com>
2024-08-30 11:45:06 -0300

    feat: create locals

---
+++ locals.tf
@@ -0,0 +1,9 @@
+locals {
+  tags = {
+    Departament  = "DevOps"
+    Organization = "Infraesturuta e Operação"
+    Project      = "ecs-iac-tf"
+    Enviroment   = "Development"
+    Author       = "Erik Nathan - @eriknathan"
+  }
+}
