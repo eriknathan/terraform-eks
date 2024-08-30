321771ce7c64ad4cecef63403159cd050148adad
eriknathan <eriknathan.contato@gmail.com>
2024-08-30 15:07:13 -0300

    feat: create public subnet and refacot vpc

---
+++ public.tf
@@ -0,0 +1,27 @@
+resource "aws_subnet" "eks_subnet_public_1a" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 1)
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "kubelabz-subnet-1a-tf"
+    }
+  )
+}
+
+resource "aws_subnet" "eks_subnet_public_1b" {
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = cidrsubnet(var.cidr_block, 8, 2)
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    local.tags,
+    {
+      Name = "kubelabz-subnet-1b-tf"
+    }
+  )
+}
