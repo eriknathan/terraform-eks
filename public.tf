cda75421bd584e0f1703327da8b2af2ba24b35bd
eriknathan <eriknathan.contato@gmail.com>
2024-09-02 14:49:57 -0300

    feat: create natgateway and eip

--- public.tf
+++ public.tf
@@ -28,7 +28,6 @@ resource "aws_subnet" "eks_subnet_public_1b" {
   )
 }
 
-
 resource "aws_route_table_association" "eks_rtb_assoc_1a" {
   subnet_id      = aws_subnet.eks_subnet_public_1a.id
   route_table_id = aws_route_table.eks_public_route_table.id
