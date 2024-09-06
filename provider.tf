8b1aa164190c2447955fc18083b0cb458f4a192e
eriknathan <eriknathan.contato@gmail.com>
2024-09-06 14:04:40 -0300

    feat: k8s provider

--- provider.tf
+++ provider.tf
@@ -4,9 +4,23 @@ terraform {
       source  = "hashicorp/aws"
       version = "5.65.0"
     }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "2.32.0"
+    }
   }
 }
 
 provider "aws" {
 
+}
+
+provider "kubernetes" {
+  host                   = module.eks_cluster.endpoint
+  cluster_ca_certificate = base64decode(module.eks_cluster.certificate_authority)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    args        = ["eks", "get-token", "--cluster-name", module.eks_cluster.cluster_name]
+    command     = "aws"
+  }
 }
